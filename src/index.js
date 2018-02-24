41f27b449e4538f5ec0a079222a6b61839d5853e
KuzmenkoLiz <lizaveta330@gmail.com>
2018-02-25 01:55:15 +0300

    result of zeros task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  zerosCount = 0;
+  while (number){
+    number = Math.floor(number/5);
+    zerosCount += number;
+  }
+  
+
+  return zerosCount;
+
 }
